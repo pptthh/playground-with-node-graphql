7ede1cb81a27d7681b0201a057aa7294f1b02253
Tóth Péter <pptthh@gmail.com>
2026-02-13 14:34:16 +0100

    test(e2e): expand Home page Cypress suite (links, deploy button, heading)

--- test/e2e/home.cy.ts
+++ test/e2e/home.cy.ts
@@ -1,6 +1,21 @@
 describe('Home page', () => {
-  it('renders and shows the main heading', () => {
-    cy.visit('/')
+  beforeEach(() => cy.visit('/'))
+
+  it('shows the main heading', () => {
     cy.contains('To get started, edit the page.tsx file.').should('be.visible')
+  })
+
+  it('contains Templates and Learning links with correct hrefs', () => {
+    cy.get('a').contains('Templates').should('have.attr', 'href').and('include', 'vercel.com/templates')
+    cy.get('a').contains('Learning').should('have.attr', 'href').and('include', 'nextjs.org/learn')
+  })
+
+  it('has Deploy Now button that opens external link and has image', () => {
+    cy.get('a').contains('Deploy Now').should('have.attr', 'href').and('include', 'vercel.com/new')
+    cy.get('a').contains('Deploy Now').find('img, svg').should('exist')
+  })
+
+  it('Documentation link is present and points to nextjs docs', () => {
+    cy.get('a').contains('Documentation').should('have.attr', 'href').and('include', 'nextjs.org/docs')
   })
 })
