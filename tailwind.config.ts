e1d924ad8a85d1c786232b230344a5432a18b8a3
Tóth Péter <pptthh@gmail.com>
2026-02-13 12:49:07 +0100

    chore(tailwind): convert tailwind.config.js → tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx,css}', './app/**/*.{js,ts,jsx,tsx,mdx,css}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
