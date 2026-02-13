a9c078705062869ee452ca9ff84ca34cd05a0afa
Tóth Péter <pptthh@gmail.com>
2026-02-13 17:08:39 +0100

    on the way fixing cypress open in wsl

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  allowCypressEnv: false,
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'test/e2e/**/*.cy.{ts,tsx}',
     supportFile: 'cypress.config.e2e.ts',
-    allowCypressEnv: false,
+    screenshotsFolder: '.generated/screenshots',
+    videosFolder: '.generated/videos',
+  },
+  component: {
+    devServer: {
+      bundler: 'webpack',
+      framework: 'next',
+    },
+    specPattern: 'test/component/**/*.cy.{ts,tsx}',
+    supportFile: 'cypress.config.component.ts',
   },
 })
