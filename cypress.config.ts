e4d6c7d8ce0b7157aae941cabc54e6840150b50c
Tóth Péter <pptthh@gmail.com>
2026-02-16 16:59:28 +0100

    Refactor GraphQL schema into modular parts and reorganize Cypress config files

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'test/e2e/**/*.cy.{ts,tsx}',
-    supportFile: 'cypress.config.e2e.ts',
+    supportFile: 'test/cfg/cypress.e2e.ts',
     screenshotsFolder: '.generated/screenshots',
     videosFolder: '.generated/videos',
   },
@@ -15,7 +15,7 @@ export default defineConfig({
       framework: 'next',
     },
     specPattern: 'test/component/**/*.cy.{ts,tsx}',
-    supportFile: 'cypress.config.component.ts',
-    indexHtmlFile: 'test/component/component-index.html',
+    supportFile: 'test/cfg/cypress.component.ts',
+    indexHtmlFile: 'test/cfg/component-index.html',
   },
 })
